612bd90748866a535923ea4837151ddc13d0b90d
Rohan <rohanreji93@gmail.com>
2014-12-18 11:18:09 +0530

    Mifare Detector
    Nfc Detector New Version for Mifare caregory tags

--- NfcDetector/src/com/example/nfcdetector/MainActivity.java
+++ NfcDetector/src/com/example/nfcdetector/MainActivity.java
@@ -2,27 +2,42 @@ package com.example.nfcdetector;
 
 
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+
+
+
+import redis.clients.jedis.Jedis;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
@@ -30,15 +45,19 @@ import android.graphics.drawable.ColorDrawable;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.os.Vibrator;
+import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
 import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -58,45 +77,25 @@ import android.widget.Toast;
         ActionBar bar = getActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#e74c3c")));
         bar.setTitle(Html.fromHtml("<font color='#000000'>NFC Detector</font>"));
-        
+              
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
 		nfcPendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, this.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 		t=(TextView)findViewById(R.id.textView1);
-        if(NfcAdapter.ACTION_NDEF_DISCOVERED.equals(this.getIntent().getAction()))
+        if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(this.getIntent().getAction()))
 		{
 			
-			   byte[] typ,payload ;
-			    NdefMessage ndefMesg;
-			/*  
-			    Tag myTag = (Tag) in.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-			    
-			    Ndef ndefTag = Ndef.get(myTag);
-			    int size = ndefTag.getMaxSize();         // tag size
-			    String type = ndefTag.getType();  // tag type
-			     ndefMesg = ndefTag.getCachedNdefMessage();*/
-			    
-			 
-			    	 Parcelable[] rawMsgs = this.getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-			          
-			            	
-			            
-			            	ndefMesg = (NdefMessage) rawMsgs[0];
-			               
-			    	
-			  
-			    NdefRecord[] ndefRecords = ndefMesg.getRecords();
-			    int len = ndefRecords.length;
-			    for (int i = 0; i < len; i++) {
-			      typ = ndefRecords[i].getType();
-			       payload = ndefRecords[i].getPayload();
-			       tagid= getTextData(payload);
-			      
-			       Toast.makeText(getApplicationContext(), tagid+" scan again",Toast.LENGTH_SHORT).show();
-			       t.setText("TagId:\n"+tagid);
-			       UploadASyncTask upload = new UploadASyncTask();
-		            upload.execute();
-			       
-			    }
+        	
+        	
+        	Tag tagFromIntent = this.getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        	  
+         	Log.d(TAG, "UID: " + bin2hex(tagFromIntent.getId()));
+         	tagid=bin2hex(tagFromIntent.getId());
+         	t.setText(tagid);
+         	Toast.makeText(getApplicationContext(),bin2hex(tagFromIntent.getId()), Toast.LENGTH_SHORT).show();
+         	UploadASyncTask upload = new UploadASyncTask();
+            upload.execute();
+        	
+			
 		}
 	
 	}
@@ -147,29 +146,60 @@ public void onNewIntent(Intent intent) {
 	Log.d(TAG, "onNewIntent");
 
 
-	if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {		
+	if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {		
 			in=intent;
-			 byte[] typ,payload ;
-			    NdefMessage ndefMesg;
-			    Parcelable[] rawMsgs = in.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-			    ndefMesg = (NdefMessage) rawMsgs[0];
+						 
+			 Tag tagFromIntent = in.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+			      	  
+       	 
+       	  
+       	  
+       	Log.d(TAG, "UID: " + bin2hex(tagFromIntent.getId()));
+   
+       	Toast.makeText(getApplicationContext(),bin2hex(tagFromIntent.getId()), Toast.LENGTH_SHORT).show();
+			 
+       	tagid=bin2hex(tagFromIntent.getId());
+     	t.setText(tagid);
+			 
+       	UploadASyncTask upload = new UploadASyncTask();
+       upload.execute();
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+			 
+//			    NdefMessage ndefMesg;
+//			    Parcelable[] rawMsgs = in.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+//			    ndefMesg = (NdefMessage) rawMsgs[0];
 			               
-			    		  
-			    NdefRecord[] ndefRecords = ndefMesg.getRecords();
-			    int len = ndefRecords.length;
-			    for (int i = 0; i < len; i++) {
-			    	typ = ndefRecords[i].getType();
-			    	payload = ndefRecords[i].getPayload();
-			    	tagid= getTextData(payload);
-			      // Toast.makeText(getApplicationContext(), tagid,Toast.LENGTH_SHORT).show();
-			       
-			   	}
-        	
-		
-				Toast.makeText(getApplicationContext(), "Connected "+tagid,Toast.LENGTH_SHORT).show();
-				 t.setText("TagId:\n"+tagid);
-				 UploadASyncTask upload = new UploadASyncTask();
-		            upload.execute();
+			    
+//					 Tag tagFromIntent = in.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+//	            	 typ=tagFromIntent.getId();
+//	            	 tagid= getTextData(typ);
+//	            	 
+//	            	 Toast.makeText(getApplicationContext(), tagid+" scan again",Toast.LENGTH_SHORT).show();
+//			    		  
+//			    NdefRecord[] ndefRecords = ndefMesg.getRecords();
+//			    int len = ndefRecords.length;
+//			    for (int i = 0; i < len; i++) {
+//			    	typ = ndefRecords[i].getType();
+//			    	payload = ndefRecords[i].getPayload();
+//			    	tagid= getTextData(payload);
+//			      // Toast.makeText(getApplicationContext(), tagid,Toast.LENGTH_SHORT).show();
+//			       
+//			   	}
+//        	
+//		
+//				Toast.makeText(getApplicationContext(), "Connected "+tagid,Toast.LENGTH_SHORT).show();
+//				 t.setText("TagId:\n"+tagid);
+//				// new RetrieveFeedTask().execute();
+//				 UploadASyncTask upload = new UploadASyncTask();
+//		            upload.execute();
+//
 			
 			
 		
@@ -180,27 +210,152 @@ public void onNewIntent(Intent intent) {
 }
 
 
-private String getTextData(byte[] payload) {
-	  if(payload == null)
-	    return null;
-	  try {
-	    String encoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
-	    int langageCodeLength = payload[0] & 0077;
-	    return new String(payload, langageCodeLength + 1, payload.length - langageCodeLength - 1, encoding);    
-	  } catch(Exception e) {
-	    e.printStackTrace();
-	  }
-	  return null;
-	}
 
 
-private void vibrate() {
-	Log.d(TAG, "vibrate");
-	
-	Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE) ;
-	vibe.vibrate(500);
+
+
+
+
+
+
+
+
+
+
+/*
+ * To set server
+ */
+
+
+static String bin2hex(byte[] data) {
+    return String.format("%0" + (data.length * 2) + "X", new BigInteger(1,data));
+}
+public void ddemo()
+{
+final EditText e=new EditText(this);
+AlertDialog.Builder alert=new AlertDialog.Builder(this);
+alert.setTitle("Set Server");
+alert.setView(e);
+e.setText(PreferenceManager.getDefaultSharedPreferences(MainActivity.this).getString("MYIP","http://ec2-54-148-0-61.us-west-2.compute.amazonaws.com:1337/attendance/pushtodb"));
+alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+public void onClick(DialogInterface dialog, int whichButton) {
+PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit().putString("MYIP",e.getText().toString()).commit();
+dialog.cancel();
+}
+});
+alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+public void onClick(DialogInterface dialog, int whichButton) {
+//Canceled.
+}
+});
+alert.show();
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Async task to call jedistrial's function setuppublisher
+ */
+//
+//class RetrieveFeedTask extends AsyncTask<Void, Void, Void> {
+//
+//    private Exception exception;
+//
+//
+//    protected void onPostExecute(String feed) {
+//        // TODO: check this.exception 
+//        // TODO: do something with the feed
+//    }
+//
+//	@Override
+//	protected Void doInBackground(Void... params) {
+//		// TODO Auto-generated method stub
+//		JedisTrial j=new JedisTrial();
+//	      
+//        j.setupPublisher();
+//		return null;
+//	}
+//
+//	
+//}
+//
+///*
+// * class to send data to redis server
+// */
+//public class JedisTrial {
+////private ArrayList<String> messageContainer = new ArrayList<String>();
+//private CountDownLatch messageReceivedLatch = new CountDownLatch(1);
+//private CountDownLatch publishLatch = new CountDownLatch(1);
+//private final String JEDIS_SERVER ="54.148.0.61";
+//private void setupPublisher() {
+//try {
+//System.out.println("Connecting");
+//System.out.println(JEDIS_SERVER);
+//Jedis jedis = new Jedis(JEDIS_SERVER,6379);
+//System.out.println("Waiting to publish");
+////publishLatch.await();
+//System.out.println("Ready to publish, waiting one sec");
+////Thread.sleep(1000);
+//System.out.println("publishing");
+//jedis.publish("ch1",tagid);
+//System.out.println("published, closing publishing connection");
+//jedis.quit();
+//System.out.println("publishing connection closed");
+//} catch (Exception e) {
+//System.out.println(">>> OH NOES Pub, " + e.getMessage());
+//e.printStackTrace();
+//}
+//}
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//private String getTextData(byte[] payload) {
+//	  if(payload == null)
+//	    return null;
+//	  try {
+//	    String encoding = ((payload[0] & 0200) == 0) ? "UTF-8" : "UTF-16";
+//	    int langageCodeLength = payload[0] & 0077;
+//	    return new String(payload, langageCodeLength + 1, payload.length - langageCodeLength - 1, encoding);    
+//	  } catch(Exception e) {
+//	    e.printStackTrace();
+//	  }
+//	  return null;
+//	}
+//
+//
+//private void vibrate() {
+//	Log.d(TAG, "vibrate");
+//	
+//	Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE) ;
+//	vibe.vibrate(500);
+//}
+//
 
 @Override
 protected void onResume() {
@@ -237,6 +392,7 @@ protected void onPause() {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+        	ddemo();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -244,11 +400,31 @@ protected void onPause() {
     
   
 /*
- * upload scanned tag to server    
+ * upload scanned tag to server by http post 
  */
-    
-
+//    
+    String responseBody ;
+    ProgressDialog pDialog;
 private class UploadASyncTask extends AsyncTask<Void,Void, Void>{
+
+	@Override
+	protected void onPostExecute(Void result) {
+		// TODO Auto-generated method stub
+		super.onPostExecute(result);
+		 if (pDialog.isShowing())
+             pDialog.dismiss();
+		//Toast.makeText(getApplicationContext(), r.toString(), Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onPreExecute() {
+		// TODO Auto-generated method stub
+		super.onPreExecute();
+		 pDialog = new ProgressDialog(MainActivity.this);
+         pDialog.setMessage("Please wait...");
+         pDialog.setCancelable(false);
+         pDialog.show();
+	}
 
 	@Override
 	protected Void doInBackground(Void... params) {
@@ -268,8 +444,14 @@ private class UploadASyncTask extends AsyncTask<Void,Void, Void>{
             	
             
                 HttpClient httpclient = new DefaultHttpClient();
+                
+               // t.setText("rohan");
+              final String JEDIS_SERVER1 = PreferenceManager.getDefaultSharedPreferences(MainActivity.this).getString("MYIP","http://ec2-54-148-0-61.us-west-2.compute.amazonaws.com:1337/attendance/pushtodb");
+             //   final String JEDIS_SERVER1 =  "http://ec2-54-148-0-61.us-west-2.compute.amazonaws.com:1337/attendance/pushtodb";
+               
+              URL url = new URL(JEDIS_SERVER1);
 
-                HttpPost httpPost = new HttpPost("https://sensomate-checkin.herokuapp.com/attendance/pushtodb");
+              HttpPost httpPost = new HttpPost(JEDIS_SERVER1);
 
                 String json = "";
 
@@ -283,16 +465,26 @@ private class UploadASyncTask extends AsyncTask<Void,Void, Void>{
                 httpPost.setHeader("Accept", "application/json");
                 httpPost.setHeader("Content-type", "application/json");
               
-
-                HttpResponse httpResponse = httpclient.execute(httpPost);
-
-                InputStream inputStream = httpResponse.getEntity().getContent();
-            //    Toast.makeText(getApplicationContext(), inputStream, Toast.LENGTH_SHORT).
+                	
+//                ResponseHandler<String> responseHandler=new BasicResponseHandler();
+//                responseBody = httpclient.execute(httpPost, responseHandler);
+//               
               
+               HttpResponse response = httpclient.execute(httpPost);
+               HttpEntity entity = response.getEntity();
+
+               String jsonString = EntityUtils.toString(entity);
+               Log.d("rear", jsonString);
+//r=httpResponse;
+               // InputStream inputStream = httpResponse.getEntity().getContent();
+         
 
             }catch(Exception e){
 
                 Log.e("ERROR IN SEVER UPLOAD", e.getMessage());
+             //   t.setText(responseBody +"\n\nto "+PreferenceManager.getDefaultSharedPreferences(MainActivity.this).getString("MYIP","http://ec2-54-148-0-61.us-west-2.compute.amazonaws.com:1337/")); 
+        	//	t.setText("hello");
+              
             }
             return null;
 
